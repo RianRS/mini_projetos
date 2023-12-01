a0d01922e6c461e62007f1faffef645a13b598f6
RianRS <rian.ssrb@gmail.com>
2023-12-01 17:47:18 -0300

    in

--- Peso_3/circulo/__init__.py
+++
@@ -1,7 +0,0 @@
-from Peso_3.circulo.aluno.base.circulo import Circulo
-from Peso_3.circulo.aluno.base.contato import Contato
-from Peso_3.circulo.cliente.icirculo_operations_manager import ICirculoOperationsManager
-from Peso_3.circulo.cliente.icirculos_manager import ICirculosManager
-from Peso_3.circulo.cliente.icontatos_manager import IContatosManager
-
-
